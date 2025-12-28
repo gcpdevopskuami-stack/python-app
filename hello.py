2ec696cd29bc946bae8e2d18d526eea001f35e77
jeetu1980 <217350592+jeetu1980@users.noreply.github.com>
2025-12-29 01:46:45 +0530

    python app

---
+++ hello.py
@@ -0,0 +1,9 @@
+def main():
+    """Main entry point of the script."""
+    
+    print(f"Hello, Welcome to Python.")
+    print(f"Hello cloud build.")
+
+if __name__ == "__main__":
+    # This block ensures main() only runs if the script is executed directly
+    main()
